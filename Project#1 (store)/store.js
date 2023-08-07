59507fefd73848624ab75d1590b25c5ad7814a05
דוד סופר <davud@h-MacBook-Air-sl-dwd.local>
2023-08-07 17:11:53 +0300

    first commit

--- Project#1 (store)/store.js
+++ Project#1 (store)/store.js
@@ -5,7 +5,7 @@ const data = [
         Quantity: 10,
         price: 109.95,
         description: 'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
-        category: "men's clothing",
+        category: "men",
         image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
         rating: { rate: 3.9, count: 120 }
     },
@@ -15,17 +15,17 @@ const data = [
         Quantity: 12,
         price: 22.3,
         description: 'Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.',
-        category: "men's clothing",
+        category: "men",
         image: 'https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg',
         rating: { rate: 4.1, count: 259 }
     },
     {
         id: '208',
-        title: 'Mens Cotton Jacket',
+        title: 'Men',
         Quantity: 18,
         price: 55.99,
         description: 'great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.',
-        category: "men's clothing",
+        category: "men",
         image: 'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
         rating: { rate: 4.7, count: 500 }
     },
@@ -35,7 +35,7 @@ const data = [
         Quantity: 22,
         price: 15.99,
         description: 'The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.',
-        category: "men's clothing",
+        category: "men",
         image: 'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
         rating: { rate: 2.1, count: 430 }
     },
@@ -145,7 +145,7 @@ const data = [
         price: 56.99,
         Quantity: 13,
         description: 'Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates',
-        category: "women's clothing",
+        category: "woman",
         image: 'https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg',
         rating: { rate: 2.6, count: 235 }
     },
@@ -155,7 +155,7 @@ const data = [
         price: 29.95,
         Quantity: 10,
         description: '100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON',
-        category: "women's clothing",
+        category: "woman",
         image: 'https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_.jpg',
         rating: { rate: 2.9, count: 340 }
     },
@@ -165,7 +165,7 @@ const data = [
         price: 39.99,
         Quantity: 26,
         description: "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.", category: "women's clothing",
-        category: "women's clothing",
+        category: "woman",
         image: 'https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg',
         rating: { rate: 3.8, count: 679 }
     },
@@ -175,7 +175,7 @@ const data = [
         price: 9.85,
         Quantity: 41,
         description: '95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem',
-        category: "women's clothing",
+        category: "woman",
         image: 'https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_.jpg',
         rating: { rate: 4.7, count: 130 }
     },
@@ -185,7 +185,7 @@ const data = [
         price: 7.95,
         Quantity: 21,
         description: '100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort',
-        category: "women's clothing",
+        category: "woman",
         image: 'https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg',
         rating: { rate: 4.5, count: 146 }
     },
@@ -195,7 +195,7 @@ const data = [
         price: 12.99,
         Quantity: 14,
         description: '95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual / Office / Beach / School / Home / Street.Season: Spring, Summer, Autumn, Winter.', category: "women's clothing",
-        category: "women's clothing",
+        category: "woman",
         image: 'https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg',
         rating: { rate: 3.6, count: 145 }
     }
@@ -208,28 +208,32 @@ const data = [
 
 
 
-
+// creating the default home page
 for (const obj of data) {
-    
-    const card = document.createElement('div');
-    card.id = "card"
 
-    const cardBottom = document.createElement('div')
-    cardBottom.id = "cardBottom"
-
-
+    //calling 'main' part of html in order to inject the data
     const main = document.getElementsByTagName('main')[0]
     main.id = "main"
-
+    
+    //card for each item holding the image on top and other info on bottom
+    const card = document.createElement('div');
+    card.id = "card"
+    
     const image = document.createElement('img');
     image.id = "image"
     image.src = obj.image
 
+    //bottom part of the card - holding the description and edit or delete button
+    const cardBottom = document.createElement('div')
+    cardBottom.id = "cardBottom"
+
     const title = document.createElement('p')
     title.textContent = obj.title
 
+    // line between the description to icon's
     const hr = document.createElement('hr')
 
+    //delete and edit icons
     let del = document.createElement('i');
     del.classList.add("material-icons");
     del.innerHTML = "delete";
@@ -239,9 +243,64 @@ for (const obj of data) {
     edit.classList.add("material-icons");
     edit.innerHTML = "edit";
     edit.classList.add("icons")
-    //  = "url('https://www.flaticon.com/free-icons/delete')"
     
+    // adding card and all above info to 'main' page
     cardBottom.append(title, hr, del, edit )
     card.append(image, cardBottom)
     main.appendChild(card)
+}
+
+// filtering by category 
+const main = document.getElementById('main');
+
+document.querySelectorAll('#nav button').forEach(button => {
+    button.addEventListener('click', () => {
+        const category = button.textContent.trim();
+        const filteredProducts = data.filter(product => product.category.toLowerCase() === category.toLowerCase());
+        displayProducts(filteredProducts);
+    });
+});
+
+function displayProducts(products) {
+    //clear previous products in order to create new cards by to selected category
+    main.innerHTML = ''; 
+
+    //for "All Products" button
+    if (products.length === 0) {
+        document.location.reload()
+    }
+
+    //creating new cards according to selected category - as on above
+    for (const obj of products) {
+        const card = document.createElement('div');
+        card.id = 'card';
+        
+        const image = document.createElement('img');
+        image.id = 'image';
+        image.src = obj.image;
+
+        const cardBottom = document.createElement('div');
+        cardBottom.id = 'cardBottom';
+
+
+        const title = document.createElement('p');
+        title.textContent = obj.title;
+
+        const hr = document.createElement('hr');
+        
+
+        // icon's
+        let del = document.createElement('i');
+        del.classList.add('material-icons', 'icons');
+        del.innerHTML = 'delete';
+
+        let edit = document.createElement('i');
+        edit.classList.add('material-icons', 'icons');
+        edit.innerHTML = 'edit';
+
+        // adding card and all above info to 'main' html
+        cardBottom.append(title, hr, del, edit);
+        card.append(image, cardBottom);
+        main.appendChild(card);
+    }
 }
